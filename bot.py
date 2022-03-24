933d243fac014427b34527ccbe4c75e10ffef0ec
Max Hübner <max.huebner36@gmail.com>
2022-03-24 19:40:11 +0100

    Inital Test

---
+++ bot.py
@@ -0,0 +1,17 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+#TOKEN = '956618442323148841'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
