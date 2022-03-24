4bcaf800ff1552835699748e23f9680281f0d870
Niklas <niklas.kotowski@rwth-aachen.de>
2022-03-24 20:22:14 +0100

    initial Cog setup

--- bot.py
+++ bot.py
@@ -1,16 +1,35 @@
 # bot.py
 import os
-
 import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
 from dotenv import load_dotenv
+import asyncio
+import sys
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+help_command = commands.DefaultHelpCommand(
+    no_category = 'Commands'
+)
+includes = ['Cogs.MainUtility']
+desc = "Manage your League of Legends Scrims with this application. Credits JohanLohr"
 
-client = discord.Client()
+bot = Bot(command_prefix='?', description=desc, help_command=help_command)
 
-@client.event
+
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
 
-client.run(TOKEN)
+if __name__ == '__main__':
+    for include in includes:
+        try:
+            bot.load_extension(include)
+        except Exception as e:
+            print(e )
+            print(f'Failed to load extension {include}', file=sys.stderr)
+
+bot.run(TOKEN)
+
